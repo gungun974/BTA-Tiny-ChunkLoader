9d31f7cef00f7654338fd5623071e30b07daef8a
Gungun974 <xfelix974@gmail.com>
2025-04-27 13:44:13 +0400

    Rename mod

--- src/main/java/gungun974/chunkloaderAddon/ChunkloaderAddon.java
+++ src/main/java/gungun974/chunkloaderAddon/ChunkloaderAddon.java
@@ -1,15 +1,14 @@
-package turniplabs.examplemod;
+package gungun974.chunkloaderAddon;
 
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import turniplabs.halplibe.util.ClientStartEntrypoint;
 import turniplabs.halplibe.util.GameStartEntrypoint;
 import turniplabs.halplibe.util.RecipeEntrypoint;
 
 
-public class ExampleMod implements ModInitializer, RecipeEntrypoint, GameStartEntrypoint {
-    public static final String MOD_ID = "examplemod";
+public class ChunkloaderAddon implements ModInitializer, RecipeEntrypoint, GameStartEntrypoint {
+    public static final String MOD_ID = "bta-cc-chunkloader-addon";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
     @Override
     public void onInitialize() {
