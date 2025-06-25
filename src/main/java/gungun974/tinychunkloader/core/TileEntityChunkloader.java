4984616da23c0fa9542ec1e46260886174ae8076
Gungun974 <xfelix974@gmail.com>
2025-06-25 17:58:30 +0400

    Add Owner on Chunkloader block

--- src/main/java/gungun974/tinychunkloader/core/TileEntityChunkloader.java
+++ src/main/java/gungun974/tinychunkloader/core/TileEntityChunkloader.java
@@ -1,14 +1,45 @@
 package gungun974.tinychunkloader.core;
 
+import com.mojang.nbt.tags.CompoundTag;
 import gungun974.tinychunkloader.helpers.ChunkLoaderManager;
 import net.minecraft.core.block.entity.TileEntity;
 import net.minecraft.core.entity.Entity;
+import net.minecraft.core.entity.player.Player;
+import net.minecraft.core.util.helper.UUIDHelper;
 import net.minecraft.core.world.World;
+import org.jetbrains.annotations.Nullable;
+import turniplabs.halplibe.helper.EnvironmentHelper;
+
+import java.util.UUID;
 
 public class TileEntityChunkloader extends TileEntity {
+
+	private @Nullable UUID owner;
+
 	@Override
 	public void tick() {
 		super.tick();
+
+		if (EnvironmentHelper.isClientWorld()) {
+			return;
+		}
+
+		if (owner == null) {
+			if (worldObj == null) {
+				return;
+			}
+
+			final Player player = worldObj.getClosestPlayer(x, y, z, 16);
+
+			if (player == null) {
+				return;
+			}
+
+			owner = player.uuid;
+
+			return;
+		}
+
 		int currentChunkX = Math.floorDiv(x, 16);
 		int currentChunkZ = Math.floorDiv(z, 16);
 
@@ -23,5 +54,19 @@ public class TileEntityChunkloader extends TileEntity {
 	@Override
 	public boolean canBeCarried(World world, Entity potentialHolder) {
 		return true;
+	}
+
+	@Override
+	public void readFromNBT(CompoundTag tag) {
+		super.readFromNBT(tag);
+
+		this.owner = UUIDHelper.readFromTag(tag, "OwnerUUID");
+	}
+
+	@Override
+	public void writeToNBT(CompoundTag tag) {
+		super.writeToNBT(tag);
+
+		UUIDHelper.writeToTag(tag, this.owner, "OwnerUUID");
 	}
 }
