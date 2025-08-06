0fe72e9ec238df20ec4757ee746056a856d320cb
Gungun974 <xfelix974@gmail.com>
2025-08-06 07:56:05 +0400

    Include servers natural loaded chunks in random tick updates

--- src/main/java/gungun974/tinychunkloader/mixin/WorldMixin.java
+++ src/main/java/gungun974/tinychunkloader/mixin/WorldMixin.java
@@ -4,12 +4,14 @@ import gungun974.tinychunkloader.helpers.ChunkLoaderManager;
 import net.minecraft.core.world.Dimension;
 import net.minecraft.core.world.World;
 import net.minecraft.core.world.chunk.ChunkCoordinate;
+import net.minecraft.core.world.chunk.ChunkCoordinates;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import turniplabs.halplibe.helper.EnvironmentHelper;
 
 import java.util.Map;
 import java.util.Set;
@@ -22,6 +24,9 @@ public abstract class WorldMixin {
 
 	@Shadow
 	public Dimension dimension;
+
+	@Shadow
+	public abstract ChunkCoordinates getSpawnPoint();
 
 	@Inject(method = "tick", at = @At("TAIL"))
 	void keepChunkLoaded(CallbackInfo ci) {
@@ -36,6 +41,24 @@ public abstract class WorldMixin {
 			positionsToUpdate.addAll(
 				chunks.keySet()
 			);
+		}
+
+		// Also include servers natural loaded chunks
+
+		if (EnvironmentHelper.isServerEnvironment()) {
+			ChunkCoordinates spawnCords = getSpawnPoint();
+			int spawnChunkX = spawnCords.x >> 4;
+			int spawnChunkZ = spawnCords.z >> 4;
+
+			int radiusChunks = 128 / 16; // 128 is the hard coded distance of the server
+
+			for (int dx = -radiusChunks; dx <= radiusChunks; dx++) {
+				for (int dz = -radiusChunks; dz <= radiusChunks; dz++) {
+					int chunkX = spawnChunkX + dx;
+					int chunkZ = spawnChunkZ + dz;
+					positionsToUpdate.add(new ChunkCoordinate(chunkX, chunkZ));
+				}
+			}
 		}
 	}
 }
